90fa3889e09798def2fd15a598aa611906617b72
Michael Tesija <mtesija@umich.edu>
2014-04-08 17:42:32 -0400

    camera update, almost working

--- Assets/Scripts/CameraControler.cs
+++ Assets/Scripts/CameraControler.cs
@@ -11,12 +11,17 @@ public class CameraControler : MonoBehaviour
 	private float zCenter = 24;
 	private float zRange = 24;
 	
-	private float fovMax = 90;
-	private float fovMin = 15;
+	private float fovMax = 110;
+	private float fovMin = 40;
 
 	private float zoomSpeed = 4;
 
-	private float minPinchSpeed = 5;
+	private float minPinchSpeed = 10;
+
+	void Awake()
+	{
+		this.transform.position = new Vector3(xCenter, 50, zCenter);
+	}
 
 	void Update()
 	{
@@ -24,18 +29,16 @@ public class CameraControler : MonoBehaviour
 		{
 			Vector2 deltaTouch = Input.GetTouch(0).deltaPosition;
 
-			this.transform.Translate(-deltaTouch.x * panSpeed, 0, -deltaTouch.y * panSpeed);
-
 			Vector3 position = this.transform.position;
 
 			float multiplier = (fovMax - camera.fieldOfView) / (fovMax - fovMin);
-			
-			position.x = Mathf.Clamp(position.x, xCenter - (xRange * multiplier), xCenter + (xRange * multiplier));
-			position.z = Mathf.Clamp(position.z, zCenter - (zRange * multiplier), zCenter - (zRange * multiplier));
+
+			position.x = Mathf.Clamp(position.x - (deltaTouch.x * panSpeed), xCenter - (xRange * multiplier), xCenter + (xRange * multiplier));
+			position.z = Mathf.Clamp(position.z - (deltaTouch.y * panSpeed), zCenter - (zRange * multiplier), zCenter + (zRange * multiplier));
 
 			this.transform.position = position;
 		}
-		else if(Input.touchCount >= 2 && (Input.GetTouch(0).phase == TouchPhase.Moved  || Input.GetTouch(1).phase == TouchPhase.Moved))
+		else if(Input.touchCount >= 2 && Input.GetTouch(0).phase == TouchPhase.Moved && Input.GetTouch(1).phase == TouchPhase.Moved)
 		{
 			float touch0Speed = Input.GetTouch(0).deltaPosition.magnitude / Input.GetTouch(0).deltaTime;
 			float touch1Speed = Input.GetTouch(1).deltaPosition.magnitude / Input.GetTouch(1).deltaTime;
@@ -47,11 +50,11 @@ public class CameraControler : MonoBehaviour
 
 				float deltaTouch = currentDistance.magnitude - previousDistance.magnitude;
 
-				if(deltaTouch < 1)
+				if(deltaTouch <= 1)
 				{
 					this.camera.fieldOfView = Mathf.Clamp(this.camera.fieldOfView + zoomSpeed, fovMin, fovMax);
 				}
-				else
+				else if(deltaTouch + 20 > 1)
 				{
 					this.camera.fieldOfView = Mathf.Clamp(this.camera.fieldOfView - zoomSpeed, fovMin, fovMax);
 				}
