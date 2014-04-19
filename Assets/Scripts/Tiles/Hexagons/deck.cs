5fbc9c86a5549ea51d8ff9d4f706f110db529376
codykraz <bassguitarninja@gmail.com>
2014-04-19 18:09:38 -0400

    deck fixed

--- Assets/Scripts/Tiles/Hexagons/deck.cs
+++ Assets/Scripts/Tiles/Hexagons/deck.cs
@@ -1,43 +1,48 @@
 using UnityEngine;
 using System.Collections;
+using System.Collections.Generic;
 
 public class deck : MonoBehaviour
 {
-	public List<devCard> cards;
+	public List<DevCard> cards;
 	private int curCard = 0;
 
-	void Shuffle(ref List<devCard> list)  
+	void Shuffle(ref List<DevCard> list)  
 	{  
 		for (int i = 0; i < 10; i++) {
-						System.Random rng = new System.Random ();  
-						int n = list.Count;  
-						while (n > 1) {  
-								n--;  
-								int k = rng.Next (n + 1);  
-								devCard value = list [k];  
-								list [k] = list [n];  
-								list [n] = value;  
-						}  
-				}
-		return list;
+			System.Random rng = new System.Random ();  
+			int n = list.Count;  
+			while (n > 1) {  
+				n--;  
+				int k = rng.Next (n + 1);  
+				devCard value = list [k];  
+				list [k] = list [n];  
+				list [n] = value;  
+			}  
+						
+		}
 	}
 
 	// Use this for initialization
 	void Awake ()
 	{
-		cards = new List<devCard>{
+		cards = new List<DevCard>{
 			RoadBuilding,RoadBuilding,
 			
 			YearOfPlenty,YearOfPlenty,
 			Monopoly, Monopoly,
 			Victory,Victory,Victory,Victory,Victory,
 			Knight,Knight,Knight,Knight,Knight,Knight,Knight,Knight,Knight,Knight,Knight,Knight,Knight,Knight
-		}
+		};
 		
 		Shuffle(cards);
 	}
 	
-	public devCard draw(){
+	public DevCard draw(){
+		if (curCard >= 25) {
+			GameObject.Find(GameObject.Find("TurnController").GetComponent<TurnControllerScript>().currentPlayer).GetComponent<PlayerScript>().knightDevCard++;
+			return Knight;
+		}
 		
 		switch (cards[curCard]){
 			case Knight:
@@ -55,11 +60,11 @@ public class deck : MonoBehaviour
 			case RoadBuilding:
 				GameObject.Find(GameObject.Find("TurnController").GetComponent<TurnControllerScript>().currentPlayer).GetComponent<PlayerScript>().roadBuildingDevCard++;
 				break;
-			case default:
+			default:
 				break;
 		}
 		
-		if (curCard >= 25) return Knight;
+		
 		return cards[curCard++];
 		
 	}
