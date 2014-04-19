62cad08756ae63cd23c3474b71c7a06491f03e00
jstickn <jstickn@umich.edu>
2014-04-19 16:35:53 -0400

    Add Actions Screen

---
+++ Assets/Scripts/UIScripts/ActionsDialog.cs
@@ -0,0 +1,61 @@
+using UnityEngine;
+using System.Collections;
+
+public class ActionsDialog : MonoBehaviour {
+	float pic_width = 70;
+	int act_winID = 2142;
+
+	float width_buffer = 20;
+	float button_height = 50;
+
+	public static bool show_act = false;
+	
+	void Start() {		
+	}
+	
+	// Use this for initialization
+	void OnGUI () {
+		//Background
+		if (show_act){
+			GUI.ModalWindow (act_winID, new Rect (Screen.width / 10, Screen.height / 10, Screen.width * 4 / 5, Screen.height * 4 / 5), ModalContents, "Actions");
+		}
+	}
+	
+	void ModalContents (int windowID)
+	{
+		GUILayout.BeginArea (new Rect (Screen.width / 10, Screen.height / 10, Screen.width * 3 / 5, Screen.height * 4 / 5));
+		GUILayout.BeginVertical ();
+
+		if(GUILayout.Button("Build", GUILayout.Height (Screen.height* 3 /25)))
+		{
+			//Build();
+		}
+		if(GUILayout.Button("Trade with Player", GUILayout.Height (Screen.height* 3 /25)))
+		{
+			//Trade();
+		}
+		if(GUILayout.Button("Exchange Resource", GUILayout.Height (Screen.height* 3 /25)))
+		{
+			//Exchange();
+		}
+		if(GUILayout.Button("Play Development Card", GUILayout.Height (Screen.height* 3 /25)))
+		{
+			//DevCard();
+		}
+		if(GUILayout.Button("Cancel", GUILayout.Height (Screen.height* 3 /25)))
+		{
+			hide ();
+		}
+
+		GUILayout.EndVertical ();
+		GUILayout.EndArea ();
+	}
+	
+	public static void show (){
+		show_act = true;
+	}
+	
+	public static void hide (){
+		show_act = false;
+	}
+}
--- Assets/Scripts/UIScripts/GSButtonGUI.cs
+++ Assets/Scripts/UIScripts/GSButtonGUI.cs
@@ -4,11 +4,9 @@ using System.Collections;
 public class GSButtonGUI : MonoBehaviour {
 
 	float width_buffer = 20;
-	float button_height = 50;
-	bool show_actions;
+	float button_height = Screen.height* 3 /25;
 
 	void Start() {
-		show_actions = false;
 	}
 
 	void OnGUI () {
@@ -16,25 +14,13 @@ public class GSButtonGUI : MonoBehaviour {
 		// Make the first button. If it is pressed, Application.Loadlevel (1) will be executed
 		if(GUI.Button(new Rect(width_buffer,Screen.height-(button_height+width_buffer),Screen.width/2-3*width_buffer/2,button_height), "Actions")) {
 			//CALL ACTION SCREEN
-			show_actions = true;
-		}
-
-		if (show_actions) {
-			GUI.ModalWindow (1094, new Rect (Screen.width/5, Screen.height/3, Screen.width*3/5, Screen.height/3), ActionModalContents,"Actions");		
+			ActionsDialog.show();
 		}
 		
 		// Make the second button.
 		if(GUI.Button(new Rect(Screen.width/2+width_buffer/2,Screen.height-(button_height+width_buffer),Screen.width/2-3*width_buffer/2,button_height), "End Turn")) {
 			//CALL END TURN FUNCTION
 			VictoryDialog.show();
-		}
-	}
-
-	void ActionModalContents (int windowID)
-	{
-		if(GUI.Button(new Rect(Screen.width/10,Screen.height*1/12,Screen.width*2/5,Screen.height*1/6), "Actions here")) {
-			//END GAME FUNCTION
-			show_actions = false;
 		}
 	}
 }
