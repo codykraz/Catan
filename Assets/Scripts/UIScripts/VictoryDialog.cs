83d771f16db88497f6ab5682ce8413af770fd4e6
jstickn <jstickn@umich.edu>
2014-04-21 13:23:57 -0400

    Someone can now win

--- Assets/Scripts/UIScripts/VictoryDialog.cs
+++ Assets/Scripts/UIScripts/VictoryDialog.cs
@@ -9,9 +9,25 @@ public class VictoryDialog : MonoBehaviour {
 	Texture AMan;
 	public static bool show_vic = false;
 
+	private PlayerScript player1;
+	private PlayerScript player2;
+	private PlayerScript player3;
+	private PlayerScript player4;
+
+	private GSButtonGUI gs;
+
+	string winner;
+
 	void Start() {
 		AMan = Resources.Load ("A-Man", typeof(Texture)) as Texture;
 
+		player1 = GameObject.Find("Player1").GetComponent<PlayerScript>();
+		player2 = GameObject.Find("Player2").GetComponent<PlayerScript>();
+		player3 = GameObject.Find("Player3").GetComponent<PlayerScript>();
+		player4 = GameObject.Find("Player4").GetComponent<PlayerScript>();
+
+		winner = "Player1";
+		gs = GetComponent<GSButtonGUI> ();
 	}
 
 	// Use this for initialization
@@ -21,11 +37,32 @@ public class VictoryDialog : MonoBehaviour {
 		if (show_vic){
 			GUI.ModalWindow (vic_winID, new Rect (Screen.width / 5, Screen.height / 3, Screen.width * 3 / 5, Screen.height / 3), ModalContents, "Victory!");
 		}
+
+		if (player1.victoryPoints >= 10) {
+			gs.show = false;
+			show ();
+			winner=player1.name;
+		}
+		else if (player2.victoryPoints >= 10) {
+			gs.show = false;
+			show ();
+			winner=player2.name;
+		}
+		else if (player3.victoryPoints >= 10) {
+			gs.show = false;
+			show ();
+			winner=player3.name;
+		}
+		else if (player4.victoryPoints >= 10) {
+			gs.show = false;
+			show ();
+			winner=player4.name;
+		}
 	}
 
 	void ModalContents (int windowID)
 	{
-		if(GUI.Button(new Rect(Screen.width/10,Screen.height*1/12,Screen.width*2/5,Screen.height*1/6), "Continue")) {
+		if(GUI.Button(new Rect(Screen.width/10,Screen.height*1/12,Screen.width*2/5,Screen.height*1/6), winner + " Wins!")) {
 			//END GAME FUNCTION
 			hide ();
 		}
